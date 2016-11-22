aba5abfb3d222717c3a99e23d5ebfad5c7d6900c
hxdhan <xiaodong.han@gmail.com>
2016-11-22 14:24:57 +0800

    Update ipdecode.pl

--- ipdecode.pl
+++ ipdecode.pl
@@ -8,9 +8,5 @@ my $ip ='192.168.2.1';
 say pack "C*", split /\./, $ip; 
 my $dec = unpack 'N', pack "C*", split /\./, $ip;
 say "IP:$ip equals $dec";
-
-my $test="192.168.1.100";
-my $Hex_ip = &ipmask_Dec2Hex($test);
-#print $Hex_ip;  #c0a80164
 say join '.', unpack "C*", pack "N*", $dec;
 say join '.', unpack "C*", pack "H*", $Hex_ip;
